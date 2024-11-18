16fd2e3150b5bfdf97006494f918186bcc777f3b
SarahVeiga <sarahveiga55@gmail.com>
2024-11-18 14:23:32 -0300

    Resolução Exercício 04

--- 4_condicionais.php
+++ 4_condicionais.php
@@ -1,8 +1,24 @@
 <?php
 
-// Digitar PHP (1º Aqui)
+//Verificar os dados do formulário
+//Utilizando o método POST
 
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    $senha = $_POST['senha'];
+
+    if($senha === '1234567') {
+        //Redireciona para a página de 'boas vindas'
+        header("Location: 4b_bem_vindo.php");
+        exit();
+    } else {
+        //Exibe mensagem de erro
+        $erro = "Senha incorreta. Tente novamente.";
+    }
+}
 ?>
+
+<!-- Parte em HTML para exibição das informações -->
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -10,17 +26,20 @@
     <title>Página de Login</title>
 </head>
 <body>
-    <h2>Digite a senha para continuar:</h2>
+    <h1>Tente realizar o acesso</h1>
+    <h2>Digite a senha para continuar</h2>
     <form method="post" action="">
         <label for="senha">Senha:</label>
         <input type="password" name="senha" required><br>
         <button type="submit">Entrar</button>
     </form>
 
+    <!-- 2º parte em PHP, retorna o valor de acordo com a condicional -->
     <?php
-
-    // Digitar PHP (2º Aqui)
-
+    //Exibe a mensagem de erro, se existir
+    if (isset($erro)) {
+        echo "<p style=color: red,'>$erro</p>";
+    }
     ?>
 </body>
 </html>
