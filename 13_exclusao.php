47b87848b4843c2fe4dba72fd60fe9e97f6e0bad
SarahVeiga <sarahveiga55@gmail.com>
2024-11-19 15:48:06 -0300

    Resolução Exercício 13

--- 13_exclusao.php
+++ 13_exclusao.php
@@ -3,7 +3,7 @@
 
 <?php
 // Conecta ao banco de dados
-$servername = "localhost";
+$servername = "localhost:3309";
 $username = "root";
 $password = "";
 $dbname = "exercicio";
@@ -14,10 +14,19 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
+// Verifica se um ID foi passado via URL para exclusão  
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
 
+    //Deleta o registro do cliente com o ID especificado
+    $sql = "DELETE FROM clientes WHERE id='$id'";
 
-// Digitar PHP + SQL (1º Aqui)
-
+    if ($conn->query($sql) === TRUE) {
+        echo "<p>Cliente excluído com sucesso!</p>";
+    } else {
+        echo "<p>Erro ao excluir cliente: " . $conn->error . "</p>";
+    }
+}
 
 // Fecha a conexão
 $conn->close();
